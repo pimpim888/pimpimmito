c38597cc00e6d8b8ee8b555c68e516160345754d
pimpim888 <79275235+pimpim888@users.noreply.github.com>
2021-02-20 11:41:39 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,91 @@
+const help = (prefix) => {
+	return `
+「 *𝙿𝚒𝙼𝙿𝚒𝙼 𝙱𝙾𝚃* 」
+
+◪ *informações*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Criador : 𝐏𝐢𝐌𝐏𝐢𝐌 𝐁𝐎𝐓
+  ❏ 𝑵𝑼́𝑴𝑬𝑹𝑶 𝑫𝑶 𝑩𝑶𝑻 https://wa.me/994407273548
+
+◪ *FAZER*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  ├─ ❏ ${prefix}tomp3
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}marvellogo
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}ninjalogo
+  ├─ ❏ ${prefix}water
+  ├─ ❏ ${prefix}firetext
+  ├─ ❏ ${prefix}logowolf
+  ├─ ❏ ${prefix}logowolf2
+  ├─ ❏ ${prefix}phlogo
+  ├─ ❏ ${prefix}glitch
+  ├─ ❏ ${prefix}neonlogo
+  ├─ ❏ ${prefix}neonlogo2
+  ├─ ❏ ${prefix}lionlogo
+  ├─ ❏ ${prefix}jokerlogo
+  ├─ ❏ ${prefix}shadow
+  ├─ ❏ ${prefix}burnpaper
+  ├─ ❏ ${prefix}coffee
+  ├─ ❏ ${prefix}lovepaper
+  ├─ ❏ ${prefix}woodblock
+  ├─ ❏ ${prefix}qowheart
+  ├─ ❏ ${prefix}mutgrass
+  ├─ ❏ ${prefix}undergocean
+  ├─ ❏ ${prefix}woodenboards
+  ├─ ❏ ${prefix}wolfmetal
+  ├─ ❏ ${prefix}metalictglow
+  ├─ ❏ ${prefix}8bit
+  ├─ ❏ ${prefix}ttp
+  ├─ ❏ ${prefix}herrypotter
+  ├─ ❏ ${prefix}pubglogo
+  └─ ❏ ${prefix}quotemaker
+◪ *DOWNLOADER*
+  │
+  ├─ ❏ ${prefix}images
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+  ├─ ❏ ${prefix}tiktok
+  └─ ❏ ${prefix}joox
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo
+◪ *SOM*
+  │
+  ├─ ❏ ${prefix}play
+  └─ ❏ ${prefix}tts
+◪ *MÚSICA*
+  │
+  ├─ ❏ ${prefix}lirik
+  └─ ❏ ${prefix}chord
+◪ *WIBU*
+  │
+  ├─ ❏ ${prefix}neonime
+  ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  ├─ ❏ ${prefix}waifu
+  ├─ ❏ ${prefix}randomanime
+  ├─ ❏ ${prefix}husbu
+  ├─ ❏ ${prefix}husbu2
+  ├─ ❏ ${prefix}wait
+  └─ ❏ ${prefix}nekonime
+◪ *DIVERSÃO*
+  │
+  ├─ ❏ ${prefix}alay
+  ├─ ❏ ${prefix}gantengcek
+  ├─ ❏ ${prefix}watak
+  ├─ ❏ ${prefix}hobby
+  ├─ ❏ ${prefix}game
+  ├─ ❏ ${prefix}bucin
+  ├─ ❏ ${prefix}trust
+  ├─ ❏ ${prefix}dare
+  └─ ❏ ${prefix}simi
+`
+}
+
+exports.help = help
